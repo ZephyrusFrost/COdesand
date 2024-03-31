cacdc93de959e416dc4c40365ab9c9799aac58cd
ZephyrusFrost <141444827+ZephyrusFrost@users.noreply.github.com>
2024-03-31 08:24:48 +0800

    Update restart.js

--- modules/commands/restart.js
+++ modules/commands/restart.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "res",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝙼𝙰𝚁𝙹𝙷𝚄𝙽 𝙱𝙰𝚈𝙻𝙾𝙽",
+	credits: "Kyle Bait-it",
 	description: "Restart Bot",
   usePrefix: false,
 	commandCategory: "system",
@@ -12,5 +12,5 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
 	const { threadID, messageID } = event;
-	return api.sendMessage(`${global.config.BOTNAME}ʙᴏᴛ ᴡɪʟʟ ʀᴇsᴛᴀʀᴛ ᴛʜᴇsᴇ ᴡɪʟʟ ᴛᴀᴋᴇ ғᴇᴡ ᴍɪɴᴜᴛᴇs/sᴇᴄᴏɴᴅs`, threadID, () => process.exit(1));
+	return api.sendMessage(` ━━━━━━━━━━━━━━━━━━━\n♻️ ${global.config.BOTNAME}ʙᴏᴛ ᴡɪʟʟ ʀᴇsᴛᴀʀᴛ ᴛʜᴇsᴇ ᴡɪʟʟ ᴛᴀᴋᴇ ғᴇᴡ ᴍɪɴᴜᴛᴇs/sᴇᴄᴏɴᴅs\n ━━━━━━━━━━━━━━━━━━━`, threadID, () => process.exit(1));
 }
